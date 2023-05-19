420397a931498431485f6a470d6e5fa9a3d87cfb
LlechiKaito <llechi0420@gmail.com>
2023-05-19 14:01:06 +0900

    herokuのためのmigrationファイル変更

--- db/migrate/20230331132750_create_comics.rb
+++
@@ -1,9 +0,0 @@
-class CreateComics < ActiveRecord::Migration[6.0]
-  def change
-    create_table :comics do |t|
-      t.string        :subtitle, null: false
-      t.references    :work,     null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20230331132750_create_works.rb
+++ db/migrate/20230331132750_create_works.rb
@@ -1,3 +1,13 @@
+# class CreateComics < ActiveRecord::Migration[6.0]
+#   def change
+#     create_table :comics do |t|
+#       t.string        :subtitle, null: false
+#       t.references    :work,     null: false, foreign_key: true
+#       t.timestamps
+#     end
+#   end
+# end
+
 class CreateWorks < ActiveRecord::Migration[6.0]
   def change
     create_table :works do |t|
