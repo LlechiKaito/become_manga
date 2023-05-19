a34f0e947d6fac26126438a0fe25385c6554baf2
LlechiKaito <llechi0420@gmail.com>
2023-05-19 13:51:33 +0900

    migrateファイルの入れ替え

--- db/migrate/20230331133526_create_works.rb
+++ db/migrate/20230331133526_create_works.rb
@@ -1,10 +1,20 @@
-class CreateWorks < ActiveRecord::Migration[6.0]
+# class CreateWorks < ActiveRecord::Migration[6.0]
+#   def change
+#     create_table :works do |t|
+#       t.string      :main_title,  null: false
+#       t.text        :explanation, null: false
+#       t.integer     :category,    null: false
+#       t.references  :user,        null: false, foreign_key: true
+#       t.timestamps
+#     end
+#   end
+# end
+
+class CreateComics < ActiveRecord::Migration[6.0]
   def change
-    create_table :works do |t|
-      t.string      :main_title,  null: false
-      t.text        :explanation, null: false
-      t.integer     :category,    null: false
-      t.references  :user,        null: false, foreign_key: true
+    create_table :comics do |t|
+      t.string        :subtitle, null: false
+      t.references    :work,     null: false, foreign_key: true
       t.timestamps
     end
   end
