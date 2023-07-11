125553556b18fa2dd3f052ce190fd85d95d6c0e7
LlechiKaito <llechi0420@gmail.com>
2023-07-11 21:26:55 +0900

    漫画投稿機能完了

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,122 +1,124 @@
-document.addEventListener('DOMContentLoaded', function(){
-  // 新規投稿・編集ページのフォームを取得
-  const postForm = document.getElementById('new_post');
-  // プレビューを表示するためのスペースを取得
-  const previewList = document.getElementById('previews');
-  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
-  if (!postForm) return null;
-  // 投稿できる枚数の制限を定義
-  const imageLimits = 50;
 
-  // プレビュー画像を生成・表示する関数
-  const buildPreviewImage = (dataIndex, blob) =>{
-    // 画像を表示するためのdiv要素を生成
-    const previewWrapper = document.createElement('div');
-    previewWrapper.setAttribute('class', 'preview');
-    previewWrapper.setAttribute('data-index', dataIndex);
 
-    // 表示する画像を生成
-    const previewImage= document.createElement('img');
-    previewImage.setAttribute('class', 'preview-image');
-    previewImage.setAttribute('src', blob);
+// document.addEventListener('DOMContentLoaded', function(){
+//   // 新規投稿・編集ページのフォームを取得
+//   const postForm = document.getElementById('new_post');
+//   // プレビューを表示するためのスペースを取得
+//   const previewList = document.getElementById('previews');
+//   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
+//   if (!postForm) return null;
+//   // 投稿できる枚数の制限を定義
+//   const imageLimits = 50;
 
-    // 削除ボタンを生成
-    const deleteButton = document.createElement("button");
-    deleteButton.setAttribute("class", "image-delete-button");
-    deleteButton.innerText = "削除";
+//   // プレビュー画像を生成・表示する関数
+//   const buildPreviewImage = (dataIndex, blob) =>{
+//     // 画像を表示するためのdiv要素を生成
+//     const previewWrapper = document.createElement('div');
+//     previewWrapper.setAttribute('class', 'preview');
+//     previewWrapper.setAttribute('data-index', dataIndex);
 
-    const newWrapper = document.createElement('div');
-    newWrapper.setAttribute('class', 'image-delete-wrapper');
+//     // 表示する画像を生成
+//     const previewImage= document.createElement('img');
+//     previewImage.setAttribute('class', 'preview-image');
+//     previewImage.setAttribute('src', blob);
 
-    // 削除ボタンをクリックしたらプレビューとfile_fieldを削除させる
-    deleteButton.addEventListener("click", () => deleteImage(dataIndex));
+//     // 削除ボタンを生成
+//     const deleteButton = document.createElement("button");
+//     deleteButton.setAttribute("class", "image-delete-button");
+//     deleteButton.innerText = "削除";
 
-    // 最後のfile_fieldを取得
-    const lastFileFields = document.querySelectorAll('.main-form-image');
-    const lastFileField = lastFileFields[lastFileFields.length - 1];
+//     const newWrapper = document.createElement('div');
+//     newWrapper.setAttribute('class', 'image-delete-wrapper');
 
-    // 生成したHTMLの要素をブラウザに表示させる
-    previewWrapper.appendChild(previewImage);
-    previewWrapper.appendChild(lastFileField);
-    newWrapper.appendChild(deleteButton);
-    previewWrapper.appendChild(newWrapper);
-    previewList.appendChild(previewWrapper);
-  };
+//     // 削除ボタンをクリックしたらプレビューとfile_fieldを削除させる
+//     deleteButton.addEventListener("click", () => deleteImage(dataIndex));
 
-  // file_fieldを生成・表示する関数
-  const buildNewFileField = () => {
+//     // 最後のfile_fieldを取得
+//     const lastFileFields = document.querySelectorAll('.main-form-image');
+//     const lastFileField = lastFileFields[lastFileFields.length - 1];
 
-    const newWrapper = document.createElement('div');
-    newWrapper.setAttribute('class', 'main-form-image');
+//     // 生成したHTMLの要素をブラウザに表示させる
+//     previewWrapper.appendChild(previewImage);
+//     previewWrapper.appendChild(lastFileField);
+//     newWrapper.appendChild(deleteButton);
+//     previewWrapper.appendChild(newWrapper);
+//     previewList.appendChild(previewWrapper);
+//   };
 
-    // 2枚目用のfile_fieldを作成
-    const newFileField = document.createElement('input');
-    newFileField.setAttribute('type', 'file');
-    newFileField.setAttribute('name', 'comic[images][]');
+//   // file_fieldを生成・表示する関数
+//   const buildNewFileField = () => {
 
-    // 最後のfile_fieldを取得
-    const lastFileFields = document.querySelectorAll('input[type="file"][name="comic[images][]"]');
-    const lastFileField = lastFileFields[lastFileFields.length - 1];
-    // nextDataIndex = 最後のfile_fieldのdata-index + 1
-    const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
-    newFileField.setAttribute('data-index', nextDataIndex);
+//     const newWrapper = document.createElement('div');
+//     newWrapper.setAttribute('class', 'main-form-image');
 
-    // 追加されたfile_fieldにchangeイベントをセット
-    newFileField.addEventListener("change", changedFileField);
+//     // 2枚目用のfile_fieldを作成
+//     const newFileField = document.createElement('input');
+//     newFileField.setAttribute('type', 'file');
+//     newFileField.setAttribute('name', 'comic[images][]');
 
-    // 生成したfile_fieldを表示
-    const fileFieldsArea = document.querySelector('.main-file');
-    newWrapper.appendChild(newFileField);
-    fileFieldsArea.appendChild(newWrapper);
-  };
+//     // 最後のfile_fieldを取得
+//     const lastFileFields = document.querySelectorAll('input[type="file"][name="comic[images][]"]');
+//     const lastFileField = lastFileFields[lastFileFields.length - 1];
+//     // nextDataIndex = 最後のfile_fieldのdata-index + 1
+//     const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
+//     newFileField.setAttribute('data-index', nextDataIndex);
 
-  // 指定したdata-indexを持つプレビューとfile_fieldを削除する
-  const deleteImage = (dataIndex) => {
-    const deletePreviewImage = document.querySelector(`.preview[data-index="${dataIndex}"]`);
-    deletePreviewImage.remove();
-    const deleteFileField = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`);
-    deleteFileField.remove();
+//     // 追加されたfile_fieldにchangeイベントをセット
+//     newFileField.addEventListener("change", changedFileField);
 
-     // 画像の枚数が最大のときに削除ボタンを押した場合、file_fieldを1つ追加する
-     const imageCount = document.querySelectorAll(".preview").length;
-     if (imageCount == imageLimits - 1) buildNewFileField();
-  };
+//     // 生成したfile_fieldを表示
+//     const fileFieldsArea = document.querySelector('.main-file');
+//     newWrapper.appendChild(newFileField);
+//     fileFieldsArea.appendChild(newWrapper);
+//   };
 
-  // input要素で値の変化が起きた際に呼び出される関数の中身
-  const changedFileField = (e) => {
-    // data-index（何番目を操作しているか）を取得
-    const dataIndex = e.target.getAttribute('data-index');
+//   // 指定したdata-indexを持つプレビューとfile_fieldを削除する
+//   const deleteImage = (dataIndex) => {
+//     const deletePreviewImage = document.querySelector(`.preview[data-index="${dataIndex}"]`);
+//     deletePreviewImage.remove();
+//     const deleteFileField = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`);
+//     deleteFileField.remove();
 
-    const file = e.target.files[0];
+//      // 画像の枚数が最大のときに削除ボタンを押した場合、file_fieldを1つ追加する
+//      const imageCount = document.querySelectorAll(".preview").length;
+//      if (imageCount == imageLimits - 1) buildNewFileField();
+//   };
 
-    // fileが空 = 何も選択しなかったのでプレビュー等を削除して終了する
-    if (!file) {
-      deleteImage(dataIndex);
-      return null;
-    };
+//   // input要素で値の変化が起きた際に呼び出される関数の中身
+//   const changedFileField = (e) => {
+//     // data-index（何番目を操作しているか）を取得
+//     const dataIndex = e.target.getAttribute('data-index');
 
-    const blob = window.URL.createObjectURL(file);
+//     const file = e.target.files[0];
 
-    // data-indexを使用して、既にプレビューが表示されているかを確認する
-    const alreadyPreview = document.querySelector(`.preview[data-index="${dataIndex}"]`);
+//     // fileが空 = 何も選択しなかったのでプレビュー等を削除して終了する
+//     if (!file) {
+//       deleteImage(dataIndex);
+//       return null;
+//     };
 
-    if (alreadyPreview) {
-      // クリックしたfile_fieldのdata-indexと、同じ番号のプレビュー画像が既に表示されている場合は、画像の差し替えのみを行う
-      const alreadyPreviewImage = alreadyPreview.querySelector("img");
-      alreadyPreviewImage.setAttribute("src", blob);
-      return null;
-    };
+//     const blob = window.URL.createObjectURL(file);
 
-    buildPreviewImage(dataIndex, blob);
+//     // data-indexを使用して、既にプレビューが表示されているかを確認する
+//     const alreadyPreview = document.querySelector(`.preview[data-index="${dataIndex}"]`);
 
-    // 画像の枚数制限に引っかからなければ、新しいfile_fieldを追加する
-    const imageCount = document.querySelectorAll(".preview").length;
-    if (imageCount < imageLimits) buildNewFileField();
-  };
+//     if (alreadyPreview) {
+//       // クリックしたfile_fieldのdata-indexと、同じ番号のプレビュー画像が既に表示されている場合は、画像の差し替えのみを行う
+//       const alreadyPreviewImage = alreadyPreview.querySelector("img");
+//       alreadyPreviewImage.setAttribute("src", blob);
+//       return null;
+//     };
 
-  // input要素を取得
-  const fileField = document.querySelector('input[type="file"][name="comic[images][]"]');
+//     buildPreviewImage(dataIndex, blob);
 
-  // input要素で値の変化が起きた際に呼び出される関数
-  fileField.addEventListener('change', changedFileField);
-});
+//     // 画像の枚数制限に引っかからなければ、新しいfile_fieldを追加する
+//     const imageCount = document.querySelectorAll(".preview").length;
+//     if (imageCount < imageLimits) buildNewFileField();
+//   };
+
+//   // input要素を取得
+//   const fileField = document.querySelector('input[type="file"][name="comic[images][]"]');
+
+//   // input要素で値の変化が起きた際に呼び出される関数
+//   fileField.addEventListener('change', changedFileField);
+// });
