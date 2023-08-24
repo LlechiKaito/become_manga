37e2a868f83d8f4df265175df2069ab5f84e5f36
LlechiKaito <llechi0420@gmail.com>
2023-08-24 23:17:17 +0900

    comicモデルのテスト完了

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -3,12 +3,17 @@ class Comic < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_one_attached :image
   has_many_attached :images
-
   has_many :read_counts, dependent: :destroy
 
+  validates :image, presence: true
   validates :images, presence: true
-
-  validates :images, length: { minimum: 1, maximum: 50, message: "は1枚以上50枚以下にしてください" }
+  validate :validate_image_count
+  def validate_image_count
+    errors.add(:images, "は1枚以上50枚以下にしてください") if images.length < 1 || images.length > 50
+  end
+  validates :subtitle, length: { minimum: 1, maximum: 100, message: "は1文字以上100文字以下にしてください" }
+  validates :work_id, presence: true
+  validates :explanation, length: { minimum: 1, maximum: 70000, message: "は1文字以上70000文字以下にしてください" }
 
     # @comic = Comic.find(params[:id])
     # # unlessは、ifの逆です。見つからなかったら実行、つまり一人１カウントって意味です。
--- spec/factories/comics.rb
+++ spec/factories/comics.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :comic do
+    subtitle { Faker::Lorem.sentence }
+    work { FactoryBot.create(:work) }
+    explanation { Faker::Lorem.sentence }
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+      item.images.attach(io: File.open('public/images/test_image.jpeg'), filename: 'test_image.jpeg')
+    end
   end
 end
