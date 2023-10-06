ce3facd57a25e890cfdbbe41bc1ab70b1b39032d
LlechiKaito <llechi0420@gmail.com>
2023-10-06 17:45:03 +0900

    book_markとトリミングのバック完了、その他

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,30 @@ class UsersController < ApplicationController
   end
 
   def edit
+    
   end
 
   def update
-    if @user.update_without_password(user_params)
-      redirect_to user_path(current_user.id), notice: 'User was successfully updated.'
+    pattern = /^data:image\/(png|jpeg|jpg|gif);base64,([A-Za-z0-9+\/]+={0,2})$/
+    if pattern =~ params[:user][:image]
+      base64_data = params[:user][:image] # POSTリクエストからBase64データを受け取る
+      base64_data = base64_data.split(',')[1] # ヘッダーを削除してBase64データのみを取得
+      binary_data = Base64.decode64(base64_data)
+      @user.image.attach(io: StringIO.new(binary_data), filename: 'user_image.png', content_type: 'image/png')
+      if @user.update_without_password(user_params)
+        redirect_to user_path(current_user.id), notice: 'User was successfully updated.'
+      else
+        render :edit
+      end
     else
-      render :edit
+      redirect_back(fallback_location: root_path)
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :profile, :image)
+    params.require(:user).permit(:nickname, :profile)
   end
 
   def user_judge
