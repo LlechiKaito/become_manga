89666c94d878402aeb1bf6593da8a84f64d7b8e3
LlechiKaito <llechi0420@gmail.com>
2023-10-25 01:07:23 +0900

    comic workの編集更新削除の機能の追加

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,4 +1,7 @@
 class ComicsController < ApplicationController
+    before_action :authenticate_user!, only: [:create, :new, :edit, :update, :destroy]
+    before_action :comic_judge, only: [:edit, :update, :destroy]
+
     def index
         @comics = Comic.includes(:work).where(work_id: params[:work_id])
         @work = Work.find(params[:work_id])
@@ -31,7 +34,32 @@ class ComicsController < ApplicationController
         # 60秒経ったら１カウント
     end
 
+    def edit
+    end
+    
+    def update
+        if @comic.update(comic_params)
+            redirect_to  work_comics_path(@work, @comic)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @comic.destroy
+        redirect_back(fallback_location: root_path)
+    end
+
     private
+
+    def comic_judge
+        if Work.find(params[:work_id]).user[:id] == current_user[:id]
+            @comic = Comic.find(params[:id])
+            @work = Work.find(params[:work_id])
+        else
+            redirect_to root_path
+        end
+    end
     
     def comic_params
         params.require(:comic).permit(:subtitle, :explanation, {images: []}).merge(work_id: params[:work_id])
